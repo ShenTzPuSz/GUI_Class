6fdfba5b714484c88450e90df4b675b23d507418
ShenTzPuSz <bobo3128tw@yahoo.com.tw>
2024-04-11 21:19:14 +0800

    TextBox

--- N09110016/N09110016/FormTextBox.Designer.cs
+++ N09110016/N09110016/FormTextBox.Designer.cs
@@ -32,6 +32,30 @@
             this.tbInput = new System.Windows.Forms.TextBox();
             this.btnInput = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tbAdd1 = new System.Windows.Forms.TextBox();
+            this.tbAdd2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbAdd = new System.Windows.Forms.Label();
+            this.tbReduce1 = new System.Windows.Forms.TextBox();
+            this.tbMultiply1 = new System.Windows.Forms.TextBox();
+            this.tbDivide1 = new System.Windows.Forms.TextBox();
+            this.tbReduce2 = new System.Windows.Forms.TextBox();
+            this.tbMultiply2 = new System.Windows.Forms.TextBox();
+            this.tbDivide2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lbReduce = new System.Windows.Forms.Label();
+            this.lbMultiply = new System.Windows.Forms.Label();
+            this.lbDivide = new System.Windows.Forms.Label();
+            this.btnReduce = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -72,11 +96,261 @@
             this.label1.Size = new System.Drawing.Size(0, 24);
             this.label1.TabIndex = 3;
             // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnAdd.Location = new System.Drawing.Point(663, 39);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(92, 36);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "計算";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // tbAdd1
+            // 
+            this.tbAdd1.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbAdd1.Location = new System.Drawing.Point(344, 39);
+            this.tbAdd1.Name = "tbAdd1";
+            this.tbAdd1.Size = new System.Drawing.Size(100, 36);
+            this.tbAdd1.TabIndex = 5;
+            // 
+            // tbAdd2
+            // 
+            this.tbAdd2.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbAdd2.Location = new System.Drawing.Point(482, 39);
+            this.tbAdd2.Name = "tbAdd2";
+            this.tbAdd2.Size = new System.Drawing.Size(100, 36);
+            this.tbAdd2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(450, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 27);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "+";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(588, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 27);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "=";
+            // 
+            // lbAdd
+            // 
+            this.lbAdd.AutoSize = true;
+            this.lbAdd.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbAdd.Location = new System.Drawing.Point(620, 42);
+            this.lbAdd.Name = "lbAdd";
+            this.lbAdd.Size = new System.Drawing.Size(26, 27);
+            this.lbAdd.TabIndex = 9;
+            this.lbAdd.Text = "?";
+            // 
+            // tbReduce1
+            // 
+            this.tbReduce1.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbReduce1.Location = new System.Drawing.Point(344, 96);
+            this.tbReduce1.Name = "tbReduce1";
+            this.tbReduce1.Size = new System.Drawing.Size(100, 36);
+            this.tbReduce1.TabIndex = 10;
+            // 
+            // tbMultiply1
+            // 
+            this.tbMultiply1.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbMultiply1.Location = new System.Drawing.Point(344, 157);
+            this.tbMultiply1.Name = "tbMultiply1";
+            this.tbMultiply1.Size = new System.Drawing.Size(100, 36);
+            this.tbMultiply1.TabIndex = 11;
+            // 
+            // tbDivide1
+            // 
+            this.tbDivide1.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbDivide1.Location = new System.Drawing.Point(344, 218);
+            this.tbDivide1.Name = "tbDivide1";
+            this.tbDivide1.Size = new System.Drawing.Size(100, 36);
+            this.tbDivide1.TabIndex = 12;
+            // 
+            // tbReduce2
+            // 
+            this.tbReduce2.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbReduce2.Location = new System.Drawing.Point(482, 96);
+            this.tbReduce2.Name = "tbReduce2";
+            this.tbReduce2.Size = new System.Drawing.Size(100, 36);
+            this.tbReduce2.TabIndex = 14;
+            // 
+            // tbMultiply2
+            // 
+            this.tbMultiply2.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbMultiply2.Location = new System.Drawing.Point(482, 157);
+            this.tbMultiply2.Name = "tbMultiply2";
+            this.tbMultiply2.Size = new System.Drawing.Size(100, 36);
+            this.tbMultiply2.TabIndex = 15;
+            // 
+            // tbDivide2
+            // 
+            this.tbDivide2.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbDivide2.Location = new System.Drawing.Point(482, 218);
+            this.tbDivide2.Name = "tbDivide2";
+            this.tbDivide2.Size = new System.Drawing.Size(100, 36);
+            this.tbDivide2.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(450, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 27);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(450, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 27);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "x";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(450, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 27);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "/";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(588, 105);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 27);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "=";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.Location = new System.Drawing.Point(588, 157);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(26, 27);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "=";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label9.Location = new System.Drawing.Point(588, 218);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(26, 27);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "=";
+            // 
+            // lbReduce
+            // 
+            this.lbReduce.AutoSize = true;
+            this.lbReduce.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbReduce.Location = new System.Drawing.Point(620, 105);
+            this.lbReduce.Name = "lbReduce";
+            this.lbReduce.Size = new System.Drawing.Size(26, 27);
+            this.lbReduce.TabIndex = 23;
+            this.lbReduce.Text = "?";
+            // 
+            // lbMultiply
+            // 
+            this.lbMultiply.AutoSize = true;
+            this.lbMultiply.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbMultiply.Location = new System.Drawing.Point(620, 157);
+            this.lbMultiply.Name = "lbMultiply";
+            this.lbMultiply.Size = new System.Drawing.Size(26, 27);
+            this.lbMultiply.TabIndex = 24;
+            this.lbMultiply.Text = "?";
+            // 
+            // lbDivide
+            // 
+            this.lbDivide.AutoSize = true;
+            this.lbDivide.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbDivide.Location = new System.Drawing.Point(620, 218);
+            this.lbDivide.Name = "lbDivide";
+            this.lbDivide.Size = new System.Drawing.Size(26, 27);
+            this.lbDivide.TabIndex = 25;
+            this.lbDivide.Text = "?";
+            // 
+            // btnReduce
+            // 
+            this.btnReduce.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnReduce.Location = new System.Drawing.Point(663, 93);
+            this.btnReduce.Name = "btnReduce";
+            this.btnReduce.Size = new System.Drawing.Size(92, 36);
+            this.btnReduce.TabIndex = 26;
+            this.btnReduce.Text = "計算";
+            this.btnReduce.UseVisualStyleBackColor = true;
+            this.btnReduce.Click += new System.EventHandler(this.btnReduce_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnMultiply.Location = new System.Drawing.Point(663, 154);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(92, 36);
+            this.btnMultiply.TabIndex = 27;
+            this.btnMultiply.Text = "計算";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnDivide.Location = new System.Drawing.Point(663, 215);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(92, 36);
+            this.btnDivide.TabIndex = 28;
+            this.btnDivide.Text = "計算";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            // 
             // FormTextBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnReduce);
+            this.Controls.Add(this.lbDivide);
+            this.Controls.Add(this.lbMultiply);
+            this.Controls.Add(this.lbReduce);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbDivide2);
+            this.Controls.Add(this.tbMultiply2);
+            this.Controls.Add(this.tbReduce2);
+            this.Controls.Add(this.tbDivide1);
+            this.Controls.Add(this.tbMultiply1);
+            this.Controls.Add(this.tbReduce1);
+            this.Controls.Add(this.lbAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbAdd2);
+            this.Controls.Add(this.tbAdd1);
+            this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnInput);
             this.Controls.Add(this.tbInput);
@@ -94,5 +368,29 @@
         private System.Windows.Forms.TextBox tbInput;
         private System.Windows.Forms.Button btnInput;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox tbAdd1;
+        private System.Windows.Forms.TextBox tbAdd2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbAdd;
+        private System.Windows.Forms.TextBox tbReduce1;
+        private System.Windows.Forms.TextBox tbMultiply1;
+        private System.Windows.Forms.TextBox tbDivide1;
+        private System.Windows.Forms.TextBox tbReduce2;
+        private System.Windows.Forms.TextBox tbMultiply2;
+        private System.Windows.Forms.TextBox tbDivide2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lbReduce;
+        private System.Windows.Forms.Label lbMultiply;
+        private System.Windows.Forms.Label lbDivide;
+        private System.Windows.Forms.Button btnReduce;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
     }
 }
--- N09110016/N09110016/FormTextBox.cs
+++ N09110016/N09110016/FormTextBox.cs
@@ -26,5 +26,39 @@ namespace N09110016
         {
             label1.Text = tbInput.Text;
         }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int add1, add2;
+            try 
+            { 
+            add1 = Int32.Parse(tbAdd1.Text);
+            add2 = Int32.Parse(tbAdd2.Text);
+            }
+            catch
+            {
+                MessageBox.Show("請輸入數字");
+                add1=0;
+                add2=0;
+            }
+            lbAdd.Text = (add1+add2).ToString();
+        }
+
+        private void btnReduce_Click(object sender, EventArgs e)
+        {
+            int reduce1, reduce2;
+            try
+            {
+                reduce1 = Int32.Parse(tbReduce1.Text);
+                reduce2 = Int32.Parse(tbReduce2.Text);
+            }
+            catch
+            {
+                MessageBox.Show("請輸入數字");
+                reduce1=0;
+                reduce2=0;
+            }
+            lbAdd.Text = (reduce1+reduce2).ToString();
+        }
     }
 }
